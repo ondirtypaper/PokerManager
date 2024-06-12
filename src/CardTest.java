fd0d2f27d5515517f0439dfd617fd895e92ac958
SungBae <ondirtypaper@gmail.com>
2024-06-13 00:18:07 +0900

    need to test getValueCode()

--- src/CardTest.java
+++ src/CardTest.java
@@ -33,30 +33,32 @@ public class CardTest {
        vf.addCard(new Card(2,'c'));
        vf.addCard(new Card(3,'c'));
        vf.addCard(new Card(4,'c'));
-       vf.addCard(new Card(5,'c'));
-       vf.addCard(new Card(6,'c'));
+       vf.addCard(new Card(7,'h'));
+       vf.addCard(new Card(7,'c'));
 
        System.out.println(vf.toString());
 
        System.out.println(vf.hasFlush());
 
+       System.out.println(vf.getValueCode());
+
        ValueFinder vf2 = new ValueFinder();
 
-       vf2.addCard(new Card(5,'s'));
-       vf2.addCard(new Card(5,'d'));
-       vf2.addCard(new Card(6,'s'));
-       vf2.addCard(new Card(7,'c'));
-       vf2.addCard(new Card(7,'s'));
-       vf2.addCard(new Card(8,'d'));
-       vf2.addCard(new Card(9,'s'));
+      //  vf2.addCard(new Card(5,'s'));
+      //  vf2.addCard(new Card(5,'d'));
+      //  vf2.addCard(new Card(6,'s'));
+      //  vf2.addCard(new Card(7,'c'));
+      //  vf2.addCard(new Card(7,'s'));
+      //  vf2.addCard(new Card(8,'d'));
+      //  vf2.addCard(new Card(9,'s'));
        
-       System.out.println(vf2.toString());
-       System.out.println("hasPair ? : " + vf2.hasPair());
-       System.out.println("hasTwoPair ? : " + vf2.hasTwoPair());
-       System.out.println("hasTriple ? : " + vf2.hasTriple());
-       System.out.println("hasFullHouse ? : " + vf2.hasFullHouse());
-       System.out.println("hasStraight ? : " + vf2.hasStraight());
-       System.out.println("hasStraightFlush ? : " + vf2.hasStraightFlush());
+      //  System.out.println(vf2.toString());
+      //  System.out.println("hasPair ? : " + vf2.hasPair());
+      //  System.out.println("hasTwoPair ? : " + vf2.hasTwoPair());
+      //  System.out.println("hasTriple ? : " + vf2.hasTriple());
+      //  System.out.println("hasFullHouse ? : " + vf2.hasFullHouse());
+      //  System.out.println("hasStraight ? : " + vf2.hasStraight());
+      //  System.out.println("hasStraightFlush ? : " + vf2.hasStraightFlush());
 
     }
 }
